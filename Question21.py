571cd0e141b624dafd8a39ee160b44a7ce6a27ea
Tarun Kushwah <tarunkushwahkt@gmail.com>
2025-09-11 23:26:40 +0530

    Binary Search

---
+++ Question21.py
@@ -0,0 +1,15 @@
+def binarySearch(l,k):
+    if len(l)==0:
+        return False
+    else:
+        mid=len(l)//2
+        if l[mid]==k:
+            return  True
+        elif l[mid]<k:
+            return binarySearch(l[mid+1:],k)
+        else:
+            return binarySearch(l[:mid],k)
+l=[10,20,30,10,20]
+l.sort()  
+print(binarySearch(l, 40))  
+print(binarySearch(l, 20))  
