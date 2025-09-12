b9cc3ddee1566d82e6101409cd2468e58e539f80
Tarun Kushwah <tarunkushwahkt@gmail.com>
2025-09-12 22:15:00 +0530

    .

--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-def binarySearch(l,k):
-    if len(l)==0:
-        return False
-    else:
-        mid=len(l)//2
-        if l[mid]==k:
-            return  True
-        elif l[mid]<k:
-            return binarySearch(l[mid+1:],k)
-        else:
-            return binarySearch(l[:mid],k)
-l=[10,20,30,10,20]
-l.sort()
-print(binarySearch(l,10))
-    
+merge="sorting or divide and conqure"
