b86bac185acbacf427e3e91d8d1fdecd8f776f19
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-02-13 17:27:57 +0530

    '

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-merge="sorting or divide and conqure"
+def generate_timetable(subjects, teachers, rooms, days, slots):
+    timetable = {}
+    for day in days:
+        timetable[day] = {}
+        for slot in slots:
+            timetable[day][slot] = None
+    
+    for subject in subjects:
+        for session in range(subject["sessions_per_week"]):
+            for day in days:
+                for slot in slots:
+                    if timetable[day][slot] is None:
+                        timetable[day][slot] = {
+                            "subject": subject["name"],
+                            "teacher": subject["teacher"],
+                            "room": rooms.pop(0)  
+                        }
+                        break
+                else:
+                    continue
+                break
+    return timetable
